242987b400f8e34d7128e5acfe728e6f1312d866
Sivan Aharon <sivan.aharon@mail.utoronto.ca>
2018-09-05 00:39:34 -0400

    Updated styling for list. Added handlebars js for template rendering. Added placeholders for complete and remove functionality

--- client.js
+++ client.js
@@ -3,6 +3,25 @@ const list = document.getElementById('todo-list');
 
 // NOTE: These are all our globally scoped functions for interacting with the server
 // This function adds a new todo from the input
+
+list_item_template = '<div class="row">'+
+        '<div class="col-sm-8">'+
+            '<p>{{title}}</p>'+
+        '</div>'+
+        '<div class="col-sm-4">'+
+        '<div class="btn-group">'+
+    '{{#if completed}}'+
+    '<button class="btn btn-primary" type="button">DONE!</button>'+
+'{{else}}'+
+            '<button class="btn btn-success" type="button" onclick="complete(\'{{title}}\')">Complete</button>'+
+    '{{/if}}'+
+            '<button class="btn btn-danger" type="button" onclick="complete(\'{{title}}\')" >X</button>'+
+    '</div>'+
+    '</div>'+
+    '</div>'
+
+
+
 function add() {
     console.warn(event);
     const input = document.getElementById('todo-input');
@@ -17,12 +36,18 @@ function add() {
     // TODO: refocus the element
 }
 
+function complete(){
+
+}
+
 function render(todo) {
-    const listItem = document.createElement('li');
-    const listItemText = document.createTextNode(todo.title);
-    listItem.appendChild(listItemText);
-    listItem.className = 'list-group-item';
-    list.append(listItem);
+    const container = document.createElement('li');
+    container.className = 'list-group-item';
+
+    const listItem = Handlebars.compile(list_item_template);
+    const compiledItem = listItem(todo);
+    container.innerHTML = compiledItem;
+    list.append(container);
 }
 
 // NOTE: These are listeners for events from the server
@@ -31,6 +56,6 @@ server.on('load', (todos) => {
     todos.forEach((todo) => render(todo));
 });
 
-server.on('load-new', (todo)=>{
+server.on('load-new', (todo) => {
     render(todo)
 })
